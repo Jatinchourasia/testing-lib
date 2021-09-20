b50c16cde9fc54406e91b6cd7474afae75a7b225
jatin19 <jatin@bitcs.in>
2021-09-20 17:17:14 +0530

    hello component added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import converter from "./src/utils/convertor/convertor";
 import validate from "./src/utils/validater/validate";
+import hellow from "./src/utils/component/hellow";
 
-const validator = { converter, validate };
+const validator = { converter, validate, hellow };
 
 export default libbs;
---
+++ src/utils/component/hellow.jsx
@@ -0,0 +1,4 @@
+function hellow() {
+  return <h1>heeey</h1>;
+}
+export default hellow;
